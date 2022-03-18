e7e15af3163bf6a0bc34852a7d17123894910a66
MHMTTPRLK <mhmttparlak@gmail.com>
2022-03-18 15:38:34 +0300

    Navbar & Todo Component Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
