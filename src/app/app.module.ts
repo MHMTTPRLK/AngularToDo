05a847b0bf86b6dcf7de0b7530d44d887f56d180
MHMTTPRLK <mhmttparlak@gmail.com>
2022-03-18 17:09:56 +0300

    Create Task && Delete Task Func

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{FormsModule ,ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -8,6 +8,12 @@ import { TodoComponent } from './todo/todo.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { RouterModule } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -18,11 +24,18 @@ import {MatCardModule} from '@angular/material/card';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     AppRoutingModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,7 @@
+import { ITask } from './../model/tasks';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  todoForm!:FormGroup;
+  tasks:ITask[]=[];
+  inprogress:ITask[]=[];
+  done:ITask[]=[];
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.todoForm=this.fb.group({
+      item :['',Validators.required]
+    })
+  }
+  addTask(){
+    this.tasks.push({
+      description:this.todoForm.value.item,
+      done:false
+    })
+  }
+  deleteTask(i:number){
+  this.tasks.splice(i,1)
+  }
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
 }
