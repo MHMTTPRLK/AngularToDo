90b9d3a8d119ab2914019793d25173fb5017a7bb
MHMTTPRLK <mhmttparlak@gmail.com>
2022-03-21 15:30:33 +0300

    Angular ToDo Project Final Commit

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -14,6 +14,8 @@ export class TodoComponent implements OnInit {
   tasks:ITask[]=[];
   inprogress:ITask[]=[];
   done:ITask[]=[];
+  updateIndex:any;
+  isEditEnabled:boolean=false;
   constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
@@ -25,11 +27,31 @@ export class TodoComponent implements OnInit {
     this.tasks.push({
       description:this.todoForm.value.item,
       done:false
-    })
+    });
+    this.todoForm.reset();
+  }
+  onEdit(item:ITask,i : number){
+    this.todoForm.controls['item'].setValue(item.description);
+    this.updateIndex=i;
+    this.isEditEnabled=true;
+  }
+  updateTask(){
+    this.tasks[this.updateIndex].description=this.todoForm.value.item;
+    this.tasks[this.updateIndex].done=false;
+    this.todoForm.reset();
+    this.updateIndex=undefined;
+    this.isEditEnabled=false;
+
   }
   deleteTask(i:number){
   this.tasks.splice(i,1)
   }
+  deleteInProgressTask(i:number){
+    this.inprogress.splice(i,1)
+    }
+    deleteDoneTask(i:number){
+      this.done.splice(i,1)
+      }
   drop(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
@@ -42,5 +64,6 @@ export class TodoComponent implements OnInit {
       );
     }
   }
+
 
 }
